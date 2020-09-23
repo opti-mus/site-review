e14239b1b1e3db2eaed117dcebe5c813cd40f29e
opti-mus <71773616+opti-mus@users.noreply.github.com>
2020-09-23 19:23:54 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,46 @@
+let list = document.querySelectorAll('.nav-menu li');
+let chevron = document.querySelectorAll('.cat__title i');
+let select = document.querySelectorAll('.header__slider__select');
+let comment = document.querySelectorAll('.header__slider__all-slide');
+let pagination = document.querySelectorAll('.pages__list ul li')
+
+function clearClass(a, i, clz) {
+    a.forEach(el => {
+        if (i.classList.contains(clz)) return false
+        else el.classList.remove(clz);
+    })
+}
+
+// document.querySelectorAll('.products__el').forEach((item, inx, arrr) => {
+//     item.onclick = function () {
+//         clearClass(arrr, item, 'myclass');
+//         this.classList.toggle('myclass');
+//     }
+// })
+
+
+document.addEventListener('click', (e) => {
+    let allTitle = document.querySelectorAll('.cat__title');
+
+    if (e.target.dataset.list) {
+        clearClass(allTitle, e.target, 'anim');
+        e.preventDefault();
+        e.target.classList.toggle('anim');
+
+        e.target.nextElementSibling.querySelectorAll('li a').forEach(el => {
+            el.onclick = (event) => {
+                event.preventDefault();
+            }
+        })
+    }
+})
+
+const hidePagination = function (pag) {
+    pag.forEach((el, inx) => {
+        if (inx == pag.length - 2 || inx == pag.length - 3 || inx == pag.length - 4) {
+            el.innerHTML = '.'
+        }
+    })
+}
+console.log(pagination);
+hidePagination(pagination)
